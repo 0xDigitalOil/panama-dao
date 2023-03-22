91de218078b21b6ce8b231a510d1080967e26af2
greatrat00 <>
2023-03-22 10:10:34 +0100

    hardcoding etherscan token holdings url

--- components/Header.tsx
+++ components/Header.tsx
@@ -57,7 +57,7 @@ export default function Header() {
           <Link
             rel="noreferer noopener noreferrer"
             target="_blank"
-            href={`${ETHERSCAN_BASEURL}/tokenholdings?a=${addresses?.treasury}`}
+            href={`https://etherscan.io/tokenholdings?a=${addresses?.treasury}`}
             className="ml-4 border hover:bg-skin-backdrop transition ease-in-out hover:scale-110 font-semibold rounded-xl px-6 h-10 flex items-center justify-around text-skin-muted border-skin-stroke"
           >
             Ξ {treasury ? formatTreasuryBalance(treasury) : "0"}
